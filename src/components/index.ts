6493e60fb89d0dc9e94ac2b0fcb3242d568f1f06
qianfengg <fred_gqf0221@163.com>
2022-06-09 23:19:27 +0800

    面包屑处理

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,12 +4,16 @@ import XtxCarousel from "@/components/carousel/index.vue";
 import XtxMore from "@/components/more/index.vue";
 import defaultImg from "@/assets/images/200.png";
 import { useIntersectionObserver } from "@vueuse/core";
+import XtxBread from "@/components/bread/index.vue";
+import XtxBreadItem from "@/components/bread/item.vue";
 
 export default {
   install(app: App) {
     app.component(XtxSkeleton.name, XtxSkeleton);
     app.component(XtxCarousel.name, XtxCarousel);
     app.component(XtxMore.name, XtxMore);
+    app.component(XtxBread.name, XtxBread);
+    app.component(XtxBreadItem.name, XtxBreadItem);
 
     app.directive("lazy", (el, { value }) => {
       const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="category">分类组件 - {{ route.params }}</div>
+  <XtxBread>
+    <XtxBreadItem>1</XtxBreadItem>
+    <XtxBreadItem>2</XtxBreadItem>
+    <XtxBreadItem>3</XtxBreadItem>
+    <XtxBreadItem>4</XtxBreadItem>
+  </XtxBread>
 </template>
 
 <script lang="ts" name="TopCategory" setup>
