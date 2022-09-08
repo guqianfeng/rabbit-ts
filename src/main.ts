df702a60865e3ce5a65f1477bd3a029cec4220c2
qianfengg <fred_gqf0221@163.com>
2022-09-08 11:25:30 +0800

    添加调试代码

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+console.log(App);
+console.log(App?.render?.());
 
 // import XtxSkeleton from '@/components/skeleton/index.vue'
 
@@ -13,8 +16,8 @@ import "@/assets/styles/common.less";
 
 const app = createApp(App);
 // app.component('XtxSkeleton', XtxSkeleton)
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 app.use(XtxUi);
 app.use(router);
 app.use(pinia);
